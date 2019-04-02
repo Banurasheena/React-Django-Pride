eae1adf305a7e916e52e6948618410ef69bc82aa
nicholasokello <nicholasokello114@gmail.com>
2019-04-02 16:35:59 +0300

    my login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Login from './components/containers/Login'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
 import 'antd/dist/antd.css';
+import {
+  BrowserRouter as Router,
+  Route,
+
+  Switch,
+ 
+} from "react-router-dom";
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <CustomLayout>
+      <Router>
+        <div>
+             
+          <Switch>         
+            
+           
+            <Route exact path="/Login/" component={Login} />
+            
+          </Switch>
 
-      </CustomLayout>
-            
-      </div>
-            
+          
+        </div>
+      </Router>
     );
   }
 }
 
-
-
 export default App;
+
+
+
+
+
---
+++ frontend/src/components/containers/Login.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+
+class Login extends Component {
+    // state = { 
+    //     email: '',
+    //     pasword: '',
+    //  }
+
+    // onchange = (e) =>{
+    //    this.setState({
+    //        [e.target.name]: e.target.value,
+    //    });
+    // }
+    
+    // onSubmit = async () => {
+    //     const response = await this.props.mutate({
+    //         variables: this.state,
+    //     });
+    //     console.log(response);
+    // }
+
+   render() {
+        return (
+
+            <div className="container">
+            <div class="card card-login mx-auto text-center bg-dark">
+            <div className="card-header mx-auto bg-dark">
+                <span> <img src="https://upload.wikimedia.org/wikipedia/en/8/8e/Farm_Pride_logo.png " className="w-75" alt="Logo" /></span>
+                
+        
+
+            </div>
+        
+            <div className="card-body">
+                <form action="" method="post">
+                <div className="input-group form-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text"><i class="fas fa-user"></i></span>
+                    </div>
+                    <input type="text" name="email" class="form-control" placeholder="Email id" />
+                </div>
+
+                <div className="input-group form-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text"><i className="fas fa-key"></i></span>
+                    </div>
+                    <input type="password" name="password" className="form-control" placeholder="Password" />
+                </div>
+
+                <div className="form-group">
+                    <input type="submit" name="btn" value="Login" className="btn btn-outline-danger float-right login_btn" />
+                </div>
+
+                </form>
+            </div>
+            </div>
+            </div>
+           
+        );
+    }
+}    
+
+ 
+export default Login;
